197fb98fd16d3a0d5f0cffd76accf4cefecb6cac
Лукин Илья Павлович <iplukin@edu.hse.ru>
2019-05-12 00:37:58 +0500

    Added

--- ConsoleApp36/Program.cs
+++ ConsoleApp36/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp36
 {
+   
     class Program
     {
+        const double Epsilon = 0.001;
+        static double Function(double x)
+        {
+            return Math.Pow(x, 4) + (0.8 * Math.Pow(x, 3)) - (0.4 * Math.Pow(x, 2)) - 1.4 * x - 1.2;
+        }
         static void Main(string[] args)
         {
+            double begin = -1.2;
+            double end = -0.5;
+            double x = end - begin;
+            double mid = (begin + end) / 2;
+
+            while(Math.Abs(Function(mid))>Epsilon)
+            {
+                x /= 2;
+                mid = begin + x;
+                if (Math.Sign(Function(begin)) == Math.Sign(Function(mid)))
+                    begin = mid;
+            }
+
+            Console.WriteLine($"Корень уравнения {mid}");
+            Console.ReadKey();
         }
     }
 }
